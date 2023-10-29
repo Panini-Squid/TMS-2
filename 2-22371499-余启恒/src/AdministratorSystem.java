b6f113adb87023598d177a8b4f838864a88ff87b
Panini-Squid <141101977+Panini-Squid@users.noreply.github.com>
2023-10-29 17:25:27 +0800

    Delete 2-22371499-余启恒 directory

--- 2-22371499-余启恒/src/AdministratorSystem.java
+++
@@ -1,5 +0,0 @@
-import java.util.HashMap;
-
-public class AdministratorSystem {
-    public static HashMap<String, Administrator> administratorList = new HashMap<>();
-}
--- 2-22371499-余启恒/src/CustomerSystem.java
+++
@@ -1,5 +0,0 @@
-import java.util.HashMap;
-
-public class CustomerSystem {
-    public static HashMap<String, Customer> customerList = new HashMap<>();
-}
